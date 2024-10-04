83cbbb7ce1878c3184eb27352b82e1a7dfdea02d
Yago César <yagocdj@gmail.com>
2024-10-04 05:19:08 -0300

    fix: ajustar o Enum de nomes da semana

--- app/src/main/java/br/edu/ifpb/pdm/oriymenu/model/data/WeekDayNames.kt
+++ app/src/main/java/br/edu/ifpb/pdm/oriymenu/model/data/WeekDayNames.kt
@@ -1,9 +1,9 @@
 package br.edu.ifpb.pdm.oriymenu.model.data
 
 enum class WeekDayNames(val dayName: String) {
-    MONDAY("segunda-feira"),
-    TUESDAY("terça-feira"),
-    WEDNESDAY("quarta-feira"),
-    THURSDAY("quinta-feira"),
-    FRIDAY("sexta-feira")
+    MONDAY("Segunda-feira"),
+    TUESDAY("Terça-feira"),
+    WEDNESDAY("Quarta-feira"),
+    THURSDAY("Quinta-feira"),
+    FRIDAY("Sexta-feira")
 }
